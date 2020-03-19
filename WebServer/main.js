0aca26cbb2ee450ab7dd4d80a46a3a0435cca94f
YunKwon Kim <dfg3790@gmail.com>
2020-03-19 16:31:30 +0900

    web Server Default Setting

---
+++ WebServer/main.js
@@ -0,0 +1,8 @@
+
+var serverWork = require('./serverWork');
+
+
+serverWork.ServerOn();
+
+
+// 서버를 작동시키는 메인 부분
---
+++ WebServer/serverWork.js
@@ -0,0 +1,50 @@
+
+const http = require('http');
+var fs = require('fs'); // 파일 읽기, 쓰기 등이 가능한 모듈
+const hostname = '0.0.0.0';
+const port = 3000;
+
+
+function onRequest(request, response){
+  console.log(request.url);
+  if(request.method == 'GET' && (request.url == '/' || request.url == '/index.html')){
+    /*
+      console.log(request.method);
+      console.log(response);
+    */
+    response.writeHead(200, {"Content-Type":"text/html"}); // 웹페이지 형식으로 출력을 할 것을 표시
+    fs.createReadStream("./index.html").pipe(response); // 같은 디렉토리에 있는 index.html을 출력하는 용도
+  } else{
+    send404Message(response);
+  }
+  
+}
+
+
+function send404Message(response){
+  response.writeHead(404, {"Content-Type":"text/plain"});
+  response.write("404 Error....");
+  response.end();
+}
+
+function ServerOn(){
+
+
+  // http 모듈을 사용, import 와 같은 거라고 보면 됨
+
+  
+
+  const server = http.createServer(onRequest);
+
+  server.listen(port, hostname, () => { 
+      // () => {} : 추가로 도는 함수를 의미
+      // listen 하면서 추가로 되는 함수를 의미
+      
+      
+    console.log(`Server running at http://${hostname}:${port}/`);
+    //서버가 가동되었을때 나타나는 메시지
+
+  });
+}
+
+module.exports.ServerOn = ServerOn;
