e3f73121cd895cdb5032425c0540454080e32f02
김윤권 <dfg3790@gmail.com>
2020-03-19 09:19:47 +0000

    https Setting Test

--- WebServer/serverWork.js
+++ WebServer/serverWork.js
@@ -1,9 +1,13 @@
 
-const http = require('http');
+const https = require('https');
 var fs = require('fs'); // 파일 읽기, 쓰기 등이 가능한 모듈
 const hostname = '0.0.0.0';
 const port = 3000;
 
+const https_options = {
+	key: fs.readFileSync('./https_key/key.pem'),
+	cert: fs.readFileSync('./https_key/cert.pem')
+};
 
 function onRequest(request, response){
   console.log(request.url);
@@ -13,12 +17,14 @@ function onRequest(request, response){
       console.log(response);
     */
     response.writeHead(200, {"Content-Type":"text/html"}); // 웹페이지 형식으로 출력을 할 것을 표시
-    fs.createReadStream("./index.html").pipe(response); // 같은 디렉토리에 있는 index.html을 출력하는 용도
+    fs.createReadStream("./res/index.html").pipe(response); // 같은 디렉토리에 있는 index.html을 출력하는 용도
   } else{
     send404Message(response);
   }
   
 }
+
+
 
 
 function send404Message(response){
@@ -34,7 +40,7 @@ function ServerOn(){
 
   
 
-  const server = http.createServer(onRequest);
+  const server = https.createServer(https_options, onRequest);
 
   server.listen(port, hostname, () => { 
       // () => {} : 추가로 도는 함수를 의미
