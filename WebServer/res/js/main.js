69abadcef87ef5271acb367246f522f4c6c3814f
김윤권 <dfg3790@gmail.com>
2020-04-05 06:46:44 +0000

    Server three.js Test

--- WebServer/res/js/main.js
+++ WebServer/res/js/main.js
@@ -1,10 +1,10 @@
-
-
 // THREE.js 에는 3가지가 필수로 필요한데 
 // Scene, Camera, renderer;
-
 // scene 생성
+	
 var scene = new THREE.Scene(); 
+
+
 
 // camera 생성, 일단은 PerspectiveCamera 로 설정
 
@@ -15,7 +15,6 @@ var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHei
 // 0.1, 1000 : 근거리 및 원거리 클리핑 평면
 //   카메라에서 멀거나 가까운 값보다 가까운 거리에 있는 물체는 렌더링 되지 않음
 //   값 비율을 조절할 것
-
 
 
 // renderer 생성
@@ -35,7 +34,7 @@ document.body.appendChild(renderer.domElement);
 var geometry = new THREE.BoxGeometry();
 
 // 색상을 지정할 material, 즉 재질이 필요, 아래는 색깔만 입힘
-var material = new THREE.MeshBasicMaterial({color : 0x00ffff});
+var material = new THREE.MeshBasicMaterial({color : 0x000fff});
 
 // 메쉬가 필요, 장면에 삽입하고 자유롭게 움직이도록 하는 것을 넣음 
 var cube = new THREE.Mesh(geometry, material);
--- WebServer/server.js
+++ WebServer/server.js
@@ -4,17 +4,26 @@ var socketio = require('socket.io');
 var ioEvents = require('./ioEvents');
 
 
+app.use(express.static(__dirname + '/res'));
+
 var server = app.listen(3000, ()=>{
   console.log('Listening at port number 3000...')
 });
 // express 웹서버 프레임워크로 3000 포트르 Listen
 
-var io = socketio.listen(server);
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/res/index.html');
+});
+
+
+
 //express 서버를 socket.io 서버로 업그레이드
+var io = socketio.listen(server);
 
 
 var Handler = new ioEvents(io);
 
-// 다음에 각종 이벤트를 컨트롤하는 함수를 호출
+// 다음에 각종 이벤트를 컨트롤하도록 함수를 호출
 Handler.ioEventHandler();
 
